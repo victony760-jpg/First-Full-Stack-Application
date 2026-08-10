79192d369d3a55d133a09570df22ae6affe5cf95
victony760-jpg <victony760@gmail.com>
2026-08-10 19:44:08 +0800

    fixed shop context errors

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
 import { BrowserRouter } from 'react-router-dom'
-import ShopContextProvider from './Context/ShopContext' // FIXED
+import ShopContextProvider from './Context/ShopContextProvider'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
